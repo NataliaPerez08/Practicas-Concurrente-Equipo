e6f28778e886e5994e8c05bf989e73c5966da985
NataliaPerez08 <natalia_perez08@comunidad.unam.mx>
2024-02-14 23:46:52 -0600

    Hilos

--- Practica2/src/main/java/kass/concurrente/Hilos.java
+++ Practica2/src/main/java/kass/concurrente/Hilos.java
@@ -52,10 +52,7 @@ public class Hilos extends Thread{
         }
         return contrasenna;
     }
-
-    public static void main(String[] args) throws Exception {
-        System.out.println("Vamos a intentar descifrar la contrasenna concurrentemente");
-        System.out.println("A cada hilo de le asignara un rango de contrasennas a probar."+"\n"+"El rango de contrasennas se determina por el numero de hilos y el numero de contrasennas a probar");
+    public static void ejecuta4Hilos() throws InterruptedException{
         // Le asiganere [7, 13] contrasennas a cada hilo
         Long inicio = System.nanoTime();
         // Inicializamos los hilos
@@ -67,6 +64,43 @@ public class Hilos extends Thread{
         h1.start();h2.start();h3.start();h4.start();
         // Esperamos a que terminen
         h1.join();h2.join();h3.join();h4.join();
+
+        Long fin = System.nanoTime();
+        Long total = fin-inicio;
+        System.out.println("TIEMPO TOTAL: " + nanoSegundoASegundo(total));
+    }
+
+    public static void main(String[] args) throws Exception {
+        System.out.println("Vamos a intentar descifrar la contrasenna concurrentemente");
+        System.out.println("A cada hilo de le asignara un rango de contrasennas a probar."+"\n"+"El rango de contrasennas se determina por el numero de hilos y el numero de contrasennas a probar");
+        // Le asiganere [7, 13] contrasennas a cada hilo
+        Long inicio = System.nanoTime();
+        // Creamo los hilos
+        Hilos[] hilos = new Hilos[Constante.HILOS];
+        // Rangos [7,9] [9,12] [12,13] [13,13]
+
+        int[] rango_1 = {7, 9};
+        int[] rango_2 = {9, 12};
+        int[] rango_3 = {12, 13};
+        int[] rango_4 = {13, 13};
+
+        for(int i = 0; i < Constante.HILOS; ++i){
+            /* No quiero repartir de forma equitativa, pero ni modo. USEMOS Modulos*/
+            if(i%4 == 0){
+                hilos[i] = new Hilos(Constante.LLAVE, Constante.ALFABETO, rango_1[0], rango_1[1]);
+            }else if(i%4 == 1){
+                hilos[i] = new Hilos(Constante.LLAVE, Constante.ALFABETO, rango_2[0], rango_2[1]);
+            }else if(i%4 == 2){
+                hilos[i] = new Hilos(Constante.LLAVE, Constante.ALFABETO, rango_3[0], rango_3[1]);
+            }else{
+                hilos[i] = new Hilos(Constante.LLAVE, Constante.ALFABETO, rango_4[0], rango_4[1]);
+            }
+            System.err.println("Hilo: " + i + " con rango: [" + hilos[i].inicio + ", " + hilos[i].fin + "]");
+            hilos[i].start();
+        }
+        for(int i = 0; i < Constante.HILOS; ++i){
+            hilos[i].join();
+        }
 
         Long fin = System.nanoTime();
         Long total = fin-inicio;
