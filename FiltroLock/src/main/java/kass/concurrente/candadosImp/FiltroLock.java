2f1fd4707697a87143fe701701b105c835aa5de7
NataliaPerez08 <natalia_perez08@comunidad.unam.mx>
2024-03-03 20:20:24 -0600

    FiltroLock

--- FiltroLock/src/main/java/kass/concurrente/candadosImp/FiltroLock.java
+++ FiltroLock/src/main/java/kass/concurrente/candadosImp/FiltroLock.java
@@ -3,19 +3,35 @@ package kass.concurrente.candadosImp;
 import kass.concurrente.candados.Lock;
 
 public class FiltroLock  implements Lock {
+    int[] nivel;
+    int[] victima;
 
     public FiltroLock(Integer capacidad){
-
+        nivel = new int[capacidad];
+        victima = new int[capacidad];
+        for(int i = 0; i < capacidad; ++i){ // Un hilo por cada nivel
+            nivel[i] = 0;
+        }
     }
 
     @Override
     public void lock() {
-
+        int me = Integer.parseInt(Thread.currentThread().getName());
+        for(int i = 1; i < nivel.length; ++i){ // Intenta con nivel 1
+            nivel[me] = i;
+            victima[i] = me;
+            for(int k = 0; k < nivel.length; ++k){
+                while((k != me) && (nivel[k] >= i && victima[i] == me)){ 
+                    System.out.printf("Hilo %d esperando a %d\n", me, k);
+                }
+            }
+        }
     }
 
     @Override
     public void unlock() {
-
+        int me = Integer.parseInt(Thread.currentThread().getName());
+        nivel[me] = 0;
     }
 
     /**
