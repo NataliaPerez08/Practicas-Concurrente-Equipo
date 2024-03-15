e8f86a399883a812deecaf9926c1a41ae61f974f
Valeria Reyes Tapia <53274531+ValeRT24@users.noreply.github.com>
2024-03-15 00:24:43 -0600

    Update Lugar.java

--- Practica4/Estacionamiento/src/main/java/kas/concurrente/modelos/Lugar.java
+++ Practica4/Estacionamiento/src/main/java/kas/concurrente/modelos/Lugar.java
@@ -8,44 +8,43 @@ package kas.concurrente.modelos;
  * @author Kassandra Mirael
  * @version 1.0
  */
+import java.util.concurrent.Semaphore;
+
 public class Lugar {
     private Integer id;
+    private boolean disponible;
+    private Semaphore semaforo;
+    private int vecesEstacionado;
 
-    /**
-     * Metodo constructor
-     * El lugar por defecto esta disponible
-     * Pueden llegar un numero n de carros en el peor de los casos
-     * veces estacionado sera el numero de veces que se han estacianado en el lugar
-     * Si llegan 2 carros y ambos se estacionan, entonces, su valor sera de 2
-     * @param id El id del Lugar
-     */
-    public Lugar(int id){
+
+    public Lugar(int id) {
+        this.id = id;
+        disponible = true;
+        semaforo = new Semaphore(1, true); // Semáforo débil
     }
 
-    /**
-     * En este metodo se simula que se estaciona
-     * PELIGRO: ESTAS ENTRANDO A LA 2da SECCION CRITICA
-     * Cambia el valor de disponible a false
-     * Y se simula que vas pastel de cumple :D
-     * Al final, imprime un texto color ROJO diciendo que va salir (Esperen instrucciones para esto)
-     * @throws InterruptedException Si algo falla
-     */
-    public void estaciona() throws InterruptedException{
-        /*
-         * Aui va tu codigo
-         */
+    public void estaciona() throws InterruptedException {
+        semaforo.acquire();
+        disponible = false;
+        System.out.println("El carro ha sido estacionado en el lugar " + id);
+        semaforo.release();
+        vePorPastel();
     }
 
-    /**
-     * En este metodo se genera la sumulación de espera
-     * Se genera un tiempo entre 1 y 5 segundos
-     * Es pseudo aleatorio
-     * @throws InterruptedException En caso de que falle
-     */
-    public void vePorPastel() throws InterruptedException{
-        /*
-         * Aqui va tu codigo
-         */
+    public void vePorPastel() throws InterruptedException {
+        Thread.sleep((long) (Math.random() * 5000) + 1000); // Tiempo entre 1 y 5 segundos
+        System.out.println("El carro ha salido para comprar un pastel.");
     }
-    
+
+    public Integer getId() {
+        return id;
+    }
+
+    public boolean getDisponible() {
+        return disponible;
+    }
+
+    public int getVecesEstacionado() {
+        return vecesEstacionado;
+    }
 }
