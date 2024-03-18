4ed15d612f9e0b67973365faedcc9bd81fa3ae04
NataliaPerez08 <natalia_perez08@comunidad.unam.mx>
2024-03-17 20:16:15 -0600

    Borre pruebas de main

--- Practica4/Cumpleannos/src/main/java/kass/concurrente/Main.java
+++ Practica4/Cumpleannos/src/main/java/kass/concurrente/Main.java
@@ -9,32 +9,4 @@ import kass.concurrente.invitados.InversionistaFiltro;
 public class Main {
     public static void main(String[] args) throws InterruptedException {
         System.out.println("Bienvenidos al Hoyo de Kassandra");//Investiguen la referencia, quien la diga primero en discord, se lleva participacion
-        Tenedor[] tenedores = new TenedorImpl[5];
-        //Inicializamos los tenedores	
-        for(int i = 0; i < 5; i++){
-            tenedores[i] = new TenedorImpl(i);
-        }
-
-        Inversionista[] inversionistas = new InversionistaFiltro[5];
-        int TAM_MESA = 5;
-
-        Thread[] hilos = new Thread[5];
-        for(int i = 0; i < 5; i++){
-            inversionistas[i] = new InversionistaFiltro();
-            inversionistas[i].setId(i);
-            inversionistas[i].setTenedorIzq(tenedores[i]);
-            inversionistas[i].setTenedorDer(tenedores[(i+1)%TAM_MESA]);
-            hilos[i] = new Thread(inversionistas[i]);
-            hilos[i].setName(String.valueOf(i));
-            System.out.println("Hilo " +hilos[i].getName() + " creado");
-            System.out.println("Inversionista " + inversionistas[i] + " creado");
-        }
-
-        for (Thread thread : hilos) {
-            thread.start();
-        }
-        for (Thread thread : hilos) {
-            thread.join();
-        }
-    }
 }
