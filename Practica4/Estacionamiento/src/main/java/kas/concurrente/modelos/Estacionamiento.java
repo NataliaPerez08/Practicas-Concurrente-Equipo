29a0e39a7a6d11c463e8acd9df3fe84b0ff9514c
Valeria Reyes Tapia <53274531+ValeRT24@users.noreply.github.com>
2024-03-20 22:57:21 -0600

    Update Estacionamiento.java

--- Practica4/Estacionamiento/src/main/java/kas/concurrente/modelos/Estacionamiento.java
+++ Practica4/Estacionamiento/src/main/java/kas/concurrente/modelos/Estacionamiento.java
@@ -1,81 +1,54 @@
 package kas.concurrente.modelos;
 
-/**
- * En esta clase se simula el estacionamiento en si
- * Posee un conjunto de arreglos de tipo Lugar (o arreglo bidimensional?)
- * Posee un entero de lugaresDisponibles (Se podra hacer por pisos?) (Habra otra manera de hacerlo mejor?)
- * @author Kassandra Mirael
- * @version 1.0
- */
 import java.util.concurrent.Semaphore;
 
 public class Estacionamiento {
-    private Lugar[][] lugares;
-    private int lugaresDisponibles;
-    private Semaphore mutex;
-    private Semaphore disponible;
 
-    public Estacionamiento(int capacidad, int pisos) {
-        lugares = new Lugar[pisos][capacidad / pisos];
-        lugaresDisponibles = capacidad;
-        mutex = new Semaphore(1);
-        disponible = new Semaphore(capacidad);
+    private final Lugar[] lugares;
+    private final int capacidad;
+    private int lugaresDisponibles;
+
+    public Estacionamiento(int capacidad) {
+        this.capacidad = capacidad;
+        this.lugares = new Lugar[capacidad];
+        this.lugaresDisponibles = capacidad;
         inicializaLugares();
     }
 
-    public Lugar[][] getLugares() {
-        return this.lugares;
+    public int getLugaresDisponibles() {
+        return lugaresDisponibles;
     }
 
-    public int getLugaresDisponibles() {
-        try {
-            mutex.acquire();
-            int disponibles = lugaresDisponibles;
-            mutex.release();
-            return disponibles;
-        } catch (InterruptedException e) {
-            Thread.currentThread().interrupt();
-            return -1;
-        }
+    public void setLugaresDisponibles(int lugaresDisponibles) {
+        this.lugaresDisponibles = lugaresDisponibles;
     }
 
     public boolean estaLleno() {
-        try {
-            mutex.acquire();
-            boolean lleno = lugaresDisponibles == 0;
-            mutex.release();
-            return lleno;
-        } catch (InterruptedException e) {
-            Thread.currentThread().interrupt();
-            return true;
-        }
+        return lugaresDisponibles == 0;
     }
 
     public void inicializaLugares() {
-        for (int i = 0; i < lugares.length; i++) {
-            for (int j = 0; j < lugares[i].length; j++) {
-                lugares[i][j] = new Lugar(i * lugares[i].length + j);
-            }
+        for (int i = 0; i < capacidad; i++) {
+            lugares[i] = new Lugar(i);
         }
     }
 
     public void entraCarro(int nombre) throws InterruptedException {
-        disponible.acquire();
         int lugar = obtenLugar();
         asignaLugar(lugar);
         System.out.println("El carro " + nombre + " ha entrado al estacionamiento.");
-        disponible.release();
     }
 
     public void asignaLugar(int lugar) throws InterruptedException {
-        int piso = lugar / lugares[0].length;
-        int espacio = lugar % lugares[0].length;
-        lugares[piso][espacio].estaciona();
+        lugares[lugar].estaciona();
     }
 
     public int obtenLugar() {
-        int randomPiso = (int) (Math.random() * lugares.length);
-        int randomEspacio = (int) (Math.random() * lugares[0].length);
-        return randomPiso * lugares[0].length + randomEspacio;
+        return (int) (Math.random() * capacidad);
+    }
+
+    // Método para obtener la lista de lugares
+    public Lugar[] getLugares() {
+        return lugares;
     }
 }
