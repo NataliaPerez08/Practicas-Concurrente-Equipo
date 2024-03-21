8721ec7ac85f445c807601afc01930189891fa40
Valeria Reyes Tapia <53274531+ValeRT24@users.noreply.github.com>
2024-03-20 22:56:43 -0600

    Update Main.java

--- Practica4/Estacionamiento/src/main/java/kas/concurrente/Main.java
+++ Practica4/Estacionamiento/src/main/java/kas/concurrente/Main.java
@@ -1,36 +1,29 @@
 package kas.concurrente;
-import java.util.ArrayList;
-import java.util.List;
 
 import kas.concurrente.modelos.Estacionamiento;
 
-/**
- * Clase principal, la usaran para SUS pruebas
- * Pueden modigicar los valores estaticos para ver como funciona
- * NO USEN VALORES EXTREMEDAMENTE ALTOS, puede alentar mucho su compu
- * AQUI EJECUTAN LA SIMULACION
- * @author Kassandra Mirael
- * @version 1.0
- */
-public class Main implements Runnable{
-    private Estacionamiento estacionamiento;
+public class Main implements Runnable {
+    private final Estacionamiento estacionamiento;
 
     public Main() {
-        estacionamiento = new Estacionamiento(200, 5); // Capacidad: 200, Pisos: 5
+        estacionamiento = new Estacionamiento(50); // Capacidad de 50 carros
     }
 
     public static void main(String[] args) throws InterruptedException {
         Main main = new Main();
-        main.simular();
+        main.runSimulation();
     }
 
-    public void simular() throws InterruptedException {
-        Thread[] hilos = new Thread[15]; // Número de carros
-        for (int i = 0; i < hilos.length; i++) {
-            hilos[i] = new Thread(this, "Carro " + i);
+    public void runSimulation() throws InterruptedException {
+        Thread[] hilos = new Thread[50]; // 50 carros
+
+        // Inicializa y arranca los hilos
+        for (int i = 0; i < 50; i++) {
+            hilos[i] = new Thread(this, "Carro " + (i + 1));
             hilos[i].start();
         }
 
+        // Espera a que todos los hilos terminen
         for (Thread hilo : hilos) {
             hilo.join();
         }
@@ -38,9 +31,9 @@ public class Main implements Runnable{
 
     @Override
     public void run() {
+        String nombreCarro = Thread.currentThread().getName();
         try {
-            int nombreCarro = Integer.parseInt(Thread.currentThread().getName().split(" ")[1]);
-            estacionamiento.entraCarro(nombreCarro);
+            estacionamiento.entraCarro(Integer.parseInt(nombreCarro.split(" ")[1]));
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
