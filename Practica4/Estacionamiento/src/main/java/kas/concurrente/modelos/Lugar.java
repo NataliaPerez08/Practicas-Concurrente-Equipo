58ce5787e0a647169a8e58d1b820631b2e8ffb8b
Valeria Reyes Tapia <53274531+ValeRT24@users.noreply.github.com>
2024-03-20 22:51:10 -0600

    Update Lugar.java

--- Practica4/Estacionamiento/src/main/java/kas/concurrente/modelos/Lugar.java
+++ Practica4/Estacionamiento/src/main/java/kas/concurrente/modelos/Lugar.java
@@ -1,50 +1,44 @@
 package kas.concurrente.modelos;
 
-/**
- * Clase que modela un Lugar
- * El lugar consta de un id
- * un booleano que nos dice si esta dispoible
- * y un objeto del tipo Semaphore (El semaforo)
- * @author Kassandra Mirael
- * @version 1.0
- */
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.Semaphore;
 
-public class Lugar {
-    private Integer id;
-    private boolean disponible;
-    private Semaphore semaforo;
-    private int vecesEstacionado;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
+public class LugarTest {
+    Semaphore semaforo;
+    Lugar lugar;
+    List<Thread> hilos;
+    static final int numHilos = 15;
 
-    public Lugar(int id) {
-        this.id = id;
-        disponible = true;
-        semaforo = new Semaphore(1, true); // Semáforo débil
+    @BeforeEach
+    void setUp() throws InterruptedException{
+        lugar = new Lugar(1);
+        semaforo = new Semaphore(1);
     }
 
-    public void estaciona() throws InterruptedException {
-        semaforo.acquire();
-        disponible = false;
-        System.out.println("El carro ha sido estacionado en el lugar " + id);
-        semaforo.release();
-        vePorPastel();
+    @Test
+  void constructorTest(){
+        // Creamos un lugar con un id arbitrario para la prueba
+        Lugar lugar = new Lugar(1);
+        // Verificamos que el lugar esté disponible inicialmente
+        assertTrue(lugar.getId() == 1 && lugar.getDisponible());
+     
     }
 
-    public void vePorPastel() throws InterruptedException {
-        Thread.sleep((long) (Math.random() * 5000) + 1000); // Tiempo entre 1 y 5 segundos
-        System.out.println("El carro ha salido para comprar un pastel.");
+    @Test
+    void estacionaTest() throws InterruptedException{
+        lugar.estaciona();
+        assertTrue(lugar.getDisponible());
     }
 
-    public Integer getId() {
-        return id;
-    }
-
-    public boolean getDisponible() {
-        return disponible;
-    }
-
-    public int getVecesEstacionado() {
-        return vecesEstacionado;
-    }
+    /**
+     * AGREGA 2 TEST MAS
+     * TEST bien hechos
+     */
 }
