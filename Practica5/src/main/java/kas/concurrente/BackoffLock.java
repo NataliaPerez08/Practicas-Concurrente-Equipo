7c4d1bf373f71d9f8c92dad6cbef913950a4f1d1
Valeria Reyes Tapia <53274531+ValeRT24@users.noreply.github.com>
2024-04-04 19:17:53 -0600

    Update BackoffLock.java

--- Practica5/src/main/java/kas/concurrente/BackoffLock.java
+++ Practica5/src/main/java/kas/concurrente/BackoffLock.java
@@ -1,17 +1,33 @@
 package kas.concurrente;
+import java.util.concurrent.atomic.AtomicReference;
+import java.util.concurrent.TimeUnit;
 
 public class BackoffLock implements Lock {
 
+    private static final int MIN_DELAY = 10;
+    private static final int MAX_DELAY = 100;
+
+    private AtomicReference<Thread> owner = new AtomicReference<>(null);
+
     @Override
     public void lock() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'lock'");
+        Thread currentThread = Thread.currentThread();
+        while (!owner.compareAndSet(null, currentThread)) {
+            backoff();
+        }
+    }
+
+    private void backoff() {
+        try {
+            Thread.sleep((long) (MIN_DELAY + Math.random() * (MAX_DELAY - MIN_DELAY)));
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+        }
     }
 
     @Override
     public void unlock() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'unlock'");
+          owner.set(null);
     }
     
 }
