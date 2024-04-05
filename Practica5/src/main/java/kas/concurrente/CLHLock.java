e50837a9fd91ed9c71f39f85a1601225e25710d6
JonaFC <101158336+JonaFC@users.noreply.github.com>
2024-04-05 00:49:18 -0600

    Update CLHLock.java

--- Practica5/src/main/java/kas/concurrente/CLHLock.java
+++ Practica5/src/main/java/kas/concurrente/CLHLock.java
@@ -1,17 +1,38 @@
 package kas.concurrente;
 
+import java.util.concurrent.atomic.AtomicReference;
+
 public class CLHLock implements Lock {
+
+    AtomicReference<QNode> tail;
+    ThreadLocal<QNode> myPred;
+    ThreadLocal<QNode> myNode;
+    class QNode{
+        volatile boolean lokced = false;
+    }
+
+    public CLHLock(){
+        tail = new AtomicReference<>(null);
+        myNode = ThreadLocal.withInitial(QNode::new);
+        myNode = ThreadLocal.withInitial(null);
+        
+    }
 
     @Override
     public void lock() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'lock'");
+        QNode qnode = myNode.get();
+        qnode.lokced = true;
+        QNode pred = tail.getAndSet(qnode);
+        myPred.set(pred);
+        while(pred.lokced){}
     }
 
     @Override
     public void unlock() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'unlock'");
+        QNode qnode = myNode.get();
+        qnode.lokced = false;
+        myNode.set(myPred.get());
+        
     }
-    
+
 }
